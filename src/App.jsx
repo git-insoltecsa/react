29a8d63c6fdff51684ceaa4d5275ff2ebcf341a3
Byron Estrada <bestrada1981@gmail.com>
2021-08-15 21:02:13 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">CRUD SIMPLE</h1>
+    </div>
+  );
+}
+
+export default App;
